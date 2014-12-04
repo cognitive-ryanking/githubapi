458b2c79be98e891a0b02664e477e0b2ab2492fb
cognitive-ryanking <hawking15>
2014-12-03 22:43:01 -0600

    Fix to be correct url for this test

--- spec/invalid_get_spec.rb
+++ spec/invalid_get_spec.rb
@@ -40,7 +40,7 @@ describe 'Invalid Requests to Return errors' do
   it 'Cannot take a bad header' do
     bad_headers = {:accept => 'fake/fake'}
     begin
-      RestClient.get(bad_url,bad_headers)
+      RestClient.get(url,bad_headers)
     rescue => e
       expect(e.response.code).to eq 415
       json = JSON.parse(e.response)
