0d0c62712514bd6dd7bac5e78497b102513bcee8
cognitive-ryanking <hawking15>
2014-12-03 22:12:29 -0600

    Returns true now instead of truthy response

--- spec/date_8061_spec.rb
+++ spec/date_8061_spec.rb
@@ -2,11 +2,8 @@ require 'spec_helper'
 
 
 describe 'Returns dates in ISO 8061 format' do
-  def is_is08061_valid(date_string)
-    if date_string =~ /^([0-9]{4})-?(1[0-2]|0[1-9])-?(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):?([0-5][0-9]):?([0-5][0-9])Z$/
-      return true
-    end
-      false
+  def is_iso8061_valid(date_string)
+     !!(date_string =~ /^([0-9]{4})-?(1[0-2]|0[1-9])-?(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):?([0-5][0-9]):?([0-5][0-9])Z$/)
   end
 
   let(:url) { 'https://api.github.com/repos/cognitive-ryanking/githubapi' }
@@ -15,8 +12,8 @@ describe 'Returns dates in ISO 8061 format' do
   let(:json) {JSON.parse(response)}
 
   it 'should do something' do
-    expect(is_is08061_valid(json["created_at"])).to eq true
-    expect(is_is08061_valid(json["updated_at"])).to eq true
-    expect(is_is08061_valid(json["pushed_at"])).to eq true
+    expect(is_iso8061_valid(json["created_at"])).to be true
+    expect(is_iso8061_valid(json["updated_at"])).to be true
+    expect(is_iso8061_valid(json["pushed_at"])).to be true
   end
 end
